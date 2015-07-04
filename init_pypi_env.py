cc33eaa2cda96220efbaebb01ca3087f492a2d43
ccreimondo <reimondo.zero@gmail.com>
2015-07-04 17:00:15 +0800

    Finish 1.0

--- init_pypi_env.py
+++ init_pypi_env.py
@@ -1,9 +1,66 @@
 #!/usr/bin/env python
 
+import os.path
+
+from ConfigParser import SafeConfigParser
 from subprocess import call
 
+dirname = os.path.dirname(__file__)
+file_prefix = "bandersnatch/bandersnatch"
+
+
 def create_bandersnatch_conf():
-    call(["mv", "bandersnatch-template.conf", "bandersnatch/bandersnatch-debug.conf"])
+    if not os.path.isfile("bandersnatch/bandersnatch-default.conf"):
+        call(["bandersnatch/bin/bandersnatch",
+              "-c", "bandersnatch/bandersnatch-default.conf", "mirror"])
+
+    parser = SafeConfigParser()
+    parser.read("bandersnatch/bandersnatch-default.conf")
+    parser.set("mirror", "directory", "/www/mirrors/pypi")
+    parser.set("mirror", "workers", "10")
+    parser.set("statistics", "access-log-pattern",
+                     "/var/log/pypi.mirrors.access.*.log")
+
+    with open(os.path.join(dirname, ''.join([file_prefix, "-debug.conf"])), 'w') as fp:
+        parser.write(fp)
+
+    parser.set("mirror", "log-config",
+                     "/www/mirrors/bin/bandersnatch/bandersnatch-log.conf")
+    with open(os.path.join(dirname, ''.join([file_prefix, ".conf"])), 'w') as fp:
+        parser.write(fp)
+
+
+def create_bandersnatch_log_conf():
+    parser = SafeConfigParser()
+    parser.add_section("loggers")
+    parser.set("loggers", "keys", "root, bandersnatch")
+
+    parser.add_section("handlers")
+    parser.set("handlers", "keys", "fileHandler")
+
+    parser.add_section("formatters")
+    parser.set("formatters", "keys", "simpleFormatter")
+
+    parser.add_section("formatter_simpleFormatter")
+    parser.set("formatter_simpleFormatter", "class", "logging.Formatter")
+    parser.set("formatter_simpleFormatter", "format", "%(asctime)s %(levelname)s: %(message)s")
+    # parser.set("formatter_simpleFormatter", "datefmt", "")
+
+    parser.add_section("logger_root")
+    parser.set("logger_root", "level", "ERROR")
+    parser.set("logger_root", "handlers", "fileHandler")
+
+    parser.add_section("logger_bandersnatch")
+    parser.set("logger_bandersnatch", "level", "ERROR")
+    parser.set("logger_bandersnatch", "handlers", "fileHandler")
+
+    parser.add_section("handler_fileHandler")
+    parser.set("handler_fileHandler", "class", "FileHandler")
+    parser.set("handler_fileHandler", "formatter", "simpleFormatter")
+    parser.set("handler_fileHandler", "args", "(\"/www/mirrors/log/pypi_error.log\", 'a')")
+
+    with open(os.path.join(dirname, ''.join([file_prefix, "-log.conf"])), 'w') as fp:
+        parser.write(fp)
 
 
 def install_virtualenv():
@@ -12,7 +69,6 @@ def install_virtualenv():
 
 def create_bandersnatch_env():
     call(["virtualenv", "bandersnatch"])
-    # call(["cd", "bandersnatch"])
     call(["bandersnatch/bin/pip", "install", "-r",
           "https://bitbucket.org/pypa/bandersnatch/raw/stable/requirements.txt"])
 
@@ -21,7 +77,13 @@ def main():
     install_virtualenv()
     create_bandersnatch_env()
     create_bandersnatch_conf()
+    create_bandersnatch_log_conf()
+
+
+def test():
+    create_bandersnatch_log_conf()
 
 
 if __name__ == "__main__":
     main()
+    # test()
--- pypi.py
+++ pypi.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
 
-import sys.argv
+import sys
 import os.path
+import logging
 
 from subprocess import call, check_call
 
-def active_log_conf():
-    if os.path.isfile("bandersnatch/bandersnatch-log.conf"):
-        return
-
-    if os.path.isfile("bandersnatch-log.conf"):
-        rc = check_call(["mv", "bandersnatch-log.conf", "bandersnatch/"])
-    else:
-        # TODO Create default log file
-        pass
+log_dir = "/www/mirrors/log/"
 
 
-def active_bandersnatch_conf():
-    if os.path.isfile("bandersnatch/bandersnatch.conf"):
-        return
-    # TODO Create bandersnatch.conf
-    pass
+def setup_pypi_logging():
+    """Logging for front-end to get sync state
+
+    Returns:
+        logger: logging instance
+    """
+    pypi_log_file = os.path.join(log_dir, "pypi.log")
+    fh = logging.FileHandler(pypi_log_file)
+    fh.setLevel(logging.INFO)
+    formatter = logging.Formatter(fmt="%(asctime)s - %(message)s",
+                                  datefmt="%Y%m%d - %H:%M:%S")
+    fh.setFormatter(formatter)
+
+    logger = logging.getLogger("pypi")
+    logger.addHandler(fh)
+    return logger
 
 
 def dry_run():
-    """Ban console log with the help of advance log configure file"""
-    active_log_conf()
-    active_bandersnatch_conf()
+    pypi_logger = setup_pypi_logging()
+
+    pypi_logger.info("SyncStart")
     for i in range(5):          # Try five times
-        rc = check_call("bandersnatch/bin/bandersnatch", "-c", "bandersnatch/bandersnatch.conf")
+        rc = check_call("bandersnatch/bin/bandersnatch",
+                        "-c", "bandersnatch/bandersnatch.conf")
+        if rc == 0:
+            pypi_logger.info("SyncSuccd - 0")
+            break
+        elif i == 4:    
+            pypi_logger.info("SyncError - 1")
+    pypi_logger.info("SyncCompt")
 
 
 def run_with_verbose():
